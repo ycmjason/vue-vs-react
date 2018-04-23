08a78ca3cce99c7878dd267459c51aad0ba2a998
Jason Yu <me@ycmjason.com>
2018-04-23 17:22:59 +0100

    basic setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+  </main>
+</template>
+
+<script>
+
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+}
+
+section {
+  box-sizing: border-box;
+}
+</style>
---
+++ src/firebase/index.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/functions';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyDqkTod2mJ3glzKOJ-haFPhEJJ2FDYbWqo",
+  authDomain: "vue-vs-react.firebaseapp.com",
+  projectId: "vue-vs-react",
+});
+
+const db = firebase.firestore();
+db.settings({
+  timestampsInSnapshots: true 
+});
+
+.get().then(s => console.log(s.));
+export default {
+  repoSnapshots: db.collection('repo-snapshots'),
+};
