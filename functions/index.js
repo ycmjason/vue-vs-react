b593e6d92d54928245ad18c65674fbb1c280f4ea
Jason Yu <me@ycmjason.com>
2018-04-25 18:17:50 +0100

    added function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const octokit = require('@octokit/rest')();
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+const makeSnapshotFromResponse = ({ full_name, forks_count, stargazers_count, subscribers_count }) => ({
+  repo: full_name,
+  forks: forks_count,
+  stars: stargazers_count,
+  watches: subscribers_count,
+  timestamp: admin.firestore.FieldValue.serverTimestamp(),
+});
+
+const getRepo = (owner, repo) => octokit.repos.get({ owner, repo }).then(({ data }) => data).then(makeSnapshotFromResponse);
+const addRepoSnapshot = snapshot => db.collection('repo-snapshots').add(snapshot);
+
+exports.makeSnapshot = functions.https.onRequest((request, response) => {
+  const promises = [
+    getRepo('vuejs', 'vue').then(addRepoSnapshot),
+    getRepo('facebook', 'react').then(addRepoSnapshot),
+  ];
+  return Promise.all(promises).then(() => response.send("Done"));
+});
